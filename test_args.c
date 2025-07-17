a4d1ed7213daa6572e1e5d82aa4004542a2565d5
noamtamari <noamtamari98@gmail.com>
2025-07-17 20:04:01 +0300

    Refactor test_args.c to improve string and character writing functions for better modularity and readability

--- test_args.c
+++ test_args.c
@@ -2,10 +2,11 @@
 
 #define STDOUT 1
 
-__attribute__((noreturn))
-void _start(int argc, char *argv[]) {
-    const char *message = "Roei and Noam Load the ELF file give them 100\n";
-    size_t len = 47; 
+// Helper function to write a string
+void write_string(const char *str) {
+    size_t len = 0;
+    while (str[len] != '\0') len++;  // Calculate length
+    
     asm volatile (
         "movl $4, %%eax\n"
         "movl $1, %%ebx\n"
@@ -13,9 +14,47 @@ void _start(int argc, char *argv[]) {
         "movl %1, %%edx\n"
         "int $0x80\n"
         :
-        : "r"(message), "r"(len)
+        : "r"(str), "r"(len)
         : "eax", "ebx", "ecx", "edx"
     );
+}
+
+// Helper function to write a single character
+void write_char(char c) {
+    asm volatile (
+        "movl $4, %%eax\n"
+        "movl $1, %%ebx\n"
+        "movl %0, %%ecx\n"
+        "movl $1, %%edx\n"
+        "int $0x80\n"
+        :
+        : "r"(&c)
+        : "eax", "ebx", "ecx", "edx"
+    );
+}
+
+__attribute__((noreturn))
+void _start(int argc, char *argv[]) {
+    write_string("=== test_args started ===\n");
+    
+    // Print argc
+    write_string("argc = ");
+    // Simple way to print argc (assumes argc < 10)
+    char argc_char = '0' + argc;
+    write_char(argc_char);
+    write_char('\n');
+    
+    // Print all arguments
+    for (int i = 0; i < argc; i++) {
+        write_string("argv[");
+        char index_char = '0' + i;
+        write_char(index_char);
+        write_string("] = \"");
+        write_string(argv[i]);
+        write_string("\"\n");
+    }
+    
+    write_string("=== test_args finished ===\n");
 
     asm volatile (
         "movl $1, %%eax\n"
